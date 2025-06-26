9e0d03e2bb5addf97206c7b0319c68243a6d6adf
Carlospobletep99 <carlo.pobletep@duocuc.cl>
2025-06-26 14:52:36 -0400

    COMPARTIR DE FORMA LOCAL

--- src/main/java/com/inventario/inventario/InventarioApplication.java
+++ src/main/java/com/inventario/inventario/InventarioApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 //SWAGGER URL: http://localhost:8080/doc/swagger-ui/index.html
+//COMPARTIR DE FORMA LOCAL (MISMA RED, DIFERENTE DISPOSITIVO): http://MIIP:8080/api/v1/productos
+//PARA OBTENER LA IP (WINDOWS): ipconfig EN CMD, ES LA IPv4
 @SpringBootApplication
 public class InventarioApplication {
 
